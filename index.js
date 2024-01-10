8a10f42e00791367e38b1fa8566070003bd96523
Alejandro Lopez <alejandro.lopez@enti.cat>
2024-01-10 19:34:48 +0100

    Modificando el servidor para implementar Web Sockets

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let static = require('node-static');
+let http = require('http');
+let ws = require('ws');
 
 // El servidor buscar recursos dentro de nuestra carpeta './public'
 let file = new static.Server('./public');
  
-require('http').createServer(function (request, response) {
+let httpServer = http.createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
 }).listen(8080);
+
+let wsServer = ws.WebSocketServer({ httpServer });
