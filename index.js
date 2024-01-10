9fa34580bc4ba0f93a8fd438eeb6a89c0c47f834
Alejandro Lopez <alejandro.lopez@enti.cat>
2024-01-10 19:55:22 +0100

    Configurando la conexión mediante Web Socket

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ let httpServer = http.createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-})
+});
 
+// Configuramos el servidor de Web Socket a través del servidor HTTP
 let wsServer = new ws.WebSocketServer({ server: httpServer });
 
+// Ponemos a escuchar el servidor HTTP
 httpServer.listen(8080);
+
+// Configuramos las peticiones que deberemos escuchar mediante el servidor de Web Sockets
+wsServer.on('connection', (conn) => {
+    console.log("[*] EVENT: Connection");
+    conn.on('message', (data) => {
+        console.log("[*] EVENT: Data recived - " + data);
+    });
+});
