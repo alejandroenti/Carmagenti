b79ca66bb5ad5fc35403f8ae4383080ae5a72b64
Alejandro Lopez <alejandro.lopez@enti.cat>
2024-01-10 18:30:49 +0100

    Spawneando dos coches en pantalla

--- public/carmagenti.js
+++ public/carmagenti.js
@@ -10,15 +10,18 @@ const config = {
 };
 
 let car;
+let car2;
 
 const game = new Phaser.Game(config);
 
 function preload () {
-    this.load.image("car1-img", "assets/PNG/Cars/car_black_1.png");
+    this.load.image("car1-img", "assets/PNG/Cars/car_blue_small_1.png");
+    this.load.image("car2-img", "assets/PNG/Cars/car_red_small_1.png");
 }
 
 function create () {
-    car = this.add.image(config.width / 2, config.height / 2, "car1-img");
+    car = this.add.image(config.width / 4, config.height / 2, "car1-img");
+    car2 = this.add.image(3 * config.width / 4, config.height / 2, "car2-img");
 }
 
 function update () {}
