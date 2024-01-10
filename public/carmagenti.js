6f27d1ce61ab5bcb984bedc6f89e0f6cd0f219ef
Alejandro Lopez <alejandro.lopez@enti.cat>
2024-01-10 19:15:44 +0100

    Moviendo uno de los coches con los cursores del teclado

--- public/carmagenti.js
+++ public/carmagenti.js
@@ -9,8 +9,20 @@ const config = {
     }
 };
 
-let car;
+// Car Speed (LINEAR AND ANGULAR)
+const CAR_SPEED = 5;
+const CAR_ROTATION = 3;
+
+// Cars
+let car1;
 let car2;
+
+// Car Angles
+let car1Angle = 0;
+let car2Angle = 0;
+
+// Input Keys
+let cursors;
 
 const game = new Phaser.Game(config);
 
@@ -20,8 +32,26 @@ function preload () {
 }
 
 function create () {
-    car = this.add.image(config.width / 4, config.height / 2, "car1-img");
+    car1 = this.add.image(config.width / 4, config.height / 2, "car1-img");
     car2 = this.add.image(3 * config.width / 4, config.height / 2, "car2-img");
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update () {}
+function update () {
+
+    // Car 2 Input Update
+    if (cursors.up.isDown) {
+        car2.y -= CAR_SPEED * Math.cos(car2Angle * Math.PI / 180);
+        car2.x += CAR_SPEED * Math.sin(car2Angle * Math.PI / 180);
+    }
+
+    if (cursors.left.isDown) {
+        car2Angle -= CAR_ROTATION
+        car2.rotation = car2Angle * Math.PI / 180;
+    }
+    else if (cursors.right.isDown) {
+        car2Angle += CAR_ROTATION
+        car2.rotation = car2Angle * Math.PI / 180;
+    }
+}
