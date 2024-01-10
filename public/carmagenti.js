bd1594777efea83a92d1d5757a3bb6c27fda77ce
Alejandro Lopez <alejandro.lopez@enti.cat>
2024-01-10 17:40:25 +0100

    Cargando una primera imagen del coche

--- public/carmagenti.js
+++ public/carmagenti.js
@@ -2,6 +2,23 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
+let car;
+
 const game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image("car1-img", "assets/PNG/Cars/car_black_1.png");
+}
+
+function create () {
+    car = this.add.image(config.width / 2, config.height / 2, "car1-img");
+}
+
+function update () {}
