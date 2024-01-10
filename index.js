6d610b940e14c6207474d850cae10eecd4d7cb74
Alejandro Lopez <alejandro.lopez@enti.cat>
2024-01-10 16:57:20 +0100

    Generando el servidor web

---
+++ index.js
@@ -0,0 +1,10 @@
+var static = require('node-static');
+
+// El servidor buscar recursos dentro de nuestra carpeta './public'
+var file = new static.Server('./public');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
